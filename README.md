2cbd80936eb737228a5d99a7d2f163aca19561e8
nawarian <nickolas@phpsp.org.br>
2024-02-19 07:45:24 +0100

    feat: project skeleton

---
+++ README.md
@@ -0,0 +1,33 @@
+Tappy Plane - NT-MTST
+---
+
+Projetinho para explorar o framework [Phaser.JS](https://phaser.io) como ferramenta de desenvolvimento
+de jogos para o [Núcleo de Tecnologia do MTST](https://nucleodetecnologia.com.br/).
+
+## Como rodar o projeto
+
+Faça o clone e instale as dependências:
+
+```
+$ git clone git@github.com:nawarian/ntmtst-tappy-plane.git
+$ cd ntmtst-tappy-plane
+$ npm install
+```
+
+Para iniciar o jogo em seu navegador rode o seguinte comando:
+
+```
+$ npm run dev
+```
+
+O jogo ficará disponível em seu [localhost:8080](http://localhost:8080/)
+
+## Licença
+
+O código do projeto `nawarian/ntmtst-tappy-plane` está disponível sob licença GPL-3.0. Você é
+livre para copiar, modificar e distribuir cópias deste código para uso pessoal ou comercial.
+
+Toda e qualquer modificação deste código DEVE ser publicada de forma aberta e sob licença GPL-3.0.
+
+Os conteúdos da pasta `assets/` não se encaixam sob licença GPL-3.0 e suas licenças devem ser
+respeitadas individualmente.
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const htmlPluginCfg = new htmlPlugin({
+  template: path.resolve(__dirname, 'index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+const tsconfigPathsPluginCfg = new TsconfigPathsPlugin();
+
+module.exports = {
+  entry: './src/game.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|svg)/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
+        },
+      },
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'ts-loader',
+      },
+      {
+        test: require.resolve('phaser'),
+        loader: 'expose-loader',
+        options: {
+          exposes: {
+            globalName: 'Phaser',
+            override: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [htmlPluginCfg],
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    host: 'localhost',
+    port: 8080,
+    open: false,
+    hot: true,
+    watchFiles: ['src/**/*.ts', 'index.html'],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [tsconfigPathsPluginCfg],
+  },
+};
